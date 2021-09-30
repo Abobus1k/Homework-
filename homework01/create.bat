0e1ea6fde121cb0df7b11611df1163a3d9d5be2d
Abobus1k <91669954+Abobus1k@users.noreply.github.com>
2021-09-30 21:53:57 +0300

    Update create.bat

--- homework01/create.bat
+++ homework01/create.bat
@@ -7,18 +7,13 @@ mkdir %1\\DiscreteMath
 echo Лекции: Погожев Сергей Владимирович. > %1\\Algem\\readme.txt
 echo Практика: Балыкина Юлия Ефимовна >> %1\\Algem\\readme.txt 
 echo Начало занятий: %date% >> %1\\Algem\\readme.txt
-rem 1
 echo Лекции: Платонов Алексей Викторович. > %1\\MathAnalys\\readme.txt 
 echo Практика: Мышков Станислав Константинович >> %1\\MathAnalys\\readme.txt
 echo Начало занятий: %date% >> %1\\MathAnalys\\readme.txt
-rem 2
 echo Лекции: Погожев Сергей Владимирович. > %1\\BasicsOfProg\\readme.txt 
 echo Начало занятий: %date% >> %1\\BasicsOfProg\\readme.txt
-rem 3
 echo Лекции: Погожев Сергей Владимирович. > %1\\DiscreteMath\\readme.txt 
 echo Практика: Воронкова Ева Боруховна >> %1\\DiscreteMath\\readme.txt
 echo Начало занятий: %date% >> %1\\DiscreteMath\\readme.txt
-rem 4
 copy op.bat %1\\BasicsOfProg
-rem 5
 copy algem.bat %1\\Algem
