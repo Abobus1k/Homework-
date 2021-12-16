c4d8b671a1407d881435eb203a3a7fb7519730bc
Abobus1k <91669954+Abobus1k@users.noreply.github.com>
2021-12-16 19:33:01 +0300

    Add files via upload

---
+++ homework02/task7.cpp
@@ -0,0 +1,23 @@
+int circleFunc(int n) {
+    int r_2 = 0;
+
+    for (int d = 1; d <= n; d += 2)
+    {
+        if (n % d == 0)
+            r_2 += d % 4 == 1 ? 1 : -1;
+    }
+    return r_2 * 4;
+}
+
+
+void task7() {
+    int R, n = 1;
+    std::cout << "Enter R: ";
+    std::cin >> R;
+
+    for (int i = 1; i <= R * R; i++)
+    {
+        n += circleFunc(i);
+    }
+    std::cout << n << std::endl;
+}
