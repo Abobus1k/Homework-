80dba6f4efa9fa3b67ab26eab979c0c6b842d7b0
Abobus1k <91669954+Abobus1k@users.noreply.github.com>
2021-12-17 15:49:43 +0300

    Update sail.cpp

--- homework02/sail.cpp
+++ homework02/sail.cpp
@@ -2,23 +2,23 @@
 int main() {
 	double x, y;
 	std::cin >> x >> y;
-	bool b = false;
-	if (((x + 1) * (x + 1) + y * y < 1) && (y <= 0))
+	bool proverka = false;
+	if (((x + 1) * (x + 1) + y * y <= 1) && (y <= 0))
 	{
-		b = true;
+		proverka = true;
 	}
-	if (((x - 1) * (x - 1) + y * y < 1) && (y <= 0))
+	if (((x - 1) * (x - 1) + y * y <= 1) && (y <= 0))
 	{
-		b = true;
+		proverka = true;
 	}
 	if ((y <= x / 2 + 2) && (x <= 0) && (x >= -1) && (y >= 0))
 	{
-		b = true;
+		proverka = true;
 	}
 	if ((y <= -2 * x + 2) && (x <= 1) && (x >= 0) && (y >= 0))
 	{
-		b = true;
+		proverka = true;
 	}
-	if (b) std::cout << "popal";
-	if (!b) std::cout << "ne popal";
+	if (proverka) std::cout << "popal";
+	if (!proverka) std::cout << "ne popal";
 }
