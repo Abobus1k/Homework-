e0fb71b8504199aa3d0d1abc9c1c0f1c77a9af5d
Abobus1k <91669954+Abobus1k@users.noreply.github.com>
2021-12-17 16:57:10 +0300

    Update task6.cpp

--- homework05/task6.cpp
+++ homework05/task6.cpp
@@ -1,3 +1,34 @@
+Новое решение
+#include <iostream>
+int main() {
+    int N;
+    std::cin >> N;
+    int* mas = new int[N];
+    for (int i = 0; i < N; ++i)
+    {
+        std::cout << i << ": ";
+        std::cin >> mas[i];
+    }
+    int d = mas[1] - mas[0];
+    if (N > 2)
+    {
+        for (int i = 2; i < N; i++)
+            if (d != mas[i-1] - mas[i-2])
+            {
+                d = 0;
+                
+            }
+    }
+    else
+    {
+        d = 0;
+    }
+    delete[] mas;
+    
+    std::cout << d;
+    return 0;
+}
+Старое решение:
 #include <iostream>
 int main() {
     int N;
