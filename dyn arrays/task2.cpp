e7a954556cdb4e61807879eba93f2b8c1d3838b6
Abobus1k <91669954+Abobus1k@users.noreply.github.com>
2021-12-17 14:30:42 +0300

    Update task2.cpp

--- dyn arrays/task2.cpp
+++ dyn arrays/task2.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<iostream>
 int length_x(int x)
 {
 	int res = 0;
@@ -11,20 +12,19 @@ int length_x(int x)
 }
 int main()
 {
-	int n, m, max = 0;
+	int n, m,max=0;
 	std::cin >> n >> m;
-	int** matrix = new int* [n];
+	int** mas = new int* [n];
 	for (int i = 0; i < n; ++i)
 	{
-		matrix[i] = new int[m];
+		mas[i] = new int[m];
 	}
 	for (int j = 0; j < m; ++j)
 	{
 		for (int i = 0; i < n; ++i)
 		{
-			matrix[i][j] = rand();
-			if (matrix[i][j] > max) 
-				max = matrix[i][j];
+			mas[i][j] = rand();
+			if (mas[i][j] > max) max = mas[i][j];
 		}
 	}
 	max = length_x(max);
@@ -32,16 +32,14 @@ int main()
 	{
 		for (int i = 0; i < n; ++i)
 		{
-			int x = length_x(matrix[i][j]);
-			for (int i=0; i<n; ++i)
-			{
-				delete[] matrix[i];
-			}
-			delete[] matrix;
-
-			std::cout << matrix[i][j];
+			int x = length_x(mas[i][j]);
+			
+			std::cout << mas[i][j];
 			for (int i = 0; i <= max - x; ++i) std::cout << " ";
 		}
 		std::cout << std::endl;
 	}
+	for (int i = 0; i < n; ++i)
+		delete[] mas[i];
+	delete[] mas[];
 }
