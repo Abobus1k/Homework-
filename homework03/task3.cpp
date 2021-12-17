1f18fdce4bfeb3f15453917bde5a2bb4e5835dab
Abobus1k <91669954+Abobus1k@users.noreply.github.com>
2021-12-17 12:24:17 +0300

    Update task3.cpp

--- homework03/task3.cpp
+++ homework03/task3.cpp
@@ -1,27 +1,23 @@
 #include <iostream>
 
-
 int main()
 {
-    std::cout << "Enter Height/2 of rhomb: ";
-    int num, i;
+    std::cout << "Enter Height/2\n ";
+    int num;
     std::cin >> num;
 
-    for (i = 1; i <= num; ++i)
+    for (int i = 1; i <= num; i++)
     {
         for (int j = 0; j < num - i; j++) std::cout << " ";
-        for (int k = 0; k < i; k++) std::cout << "* ";
+        for (int k = 0; k < i * 2 - 1; k++) std::cout << "*";
         std::cout << std::endl;
-
     }
 
-    i -= 2;
-    for (; i >= 0; --i)
+    for (int i=num-2; i >= 0; i--)
     {
-        for (int j = 0; j < num - i; ++j) std::cout << " ";
-        for (int k = 0; k < i; ++k) std::cout << "* ";
+        for (int j = 0; j < num - i; j++) std::cout << " ";
+        for (int k = 0; k < i * 2 - 1; k++) std::cout << "*";
         std::cout << std::endl;
-
     }
     return 0;
 }
