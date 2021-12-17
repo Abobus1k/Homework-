9ad88fc7ba395e56e52c8d2bb00808ab3c40a43c
Abobus1k <91669954+Abobus1k@users.noreply.github.com>
2021-12-17 12:33:14 +0300

    Update task5.cpp

--- homework03/task5.cpp
+++ homework03/task5.cpp
@@ -3,36 +3,38 @@
 
 int main()
 {
-    std::cout << "Enter Height/2 of rhomb: ";
-    int num, i;
+    std::cout << "Enter Height/2\n";
+    int num;
     std::cin >> num;
-    std::cout << "Enter amount of rhombs: ";
-    int amount;
-    std::cin >> amount;
+    std::cout << "Enter quantity\n";
+    int quan;
+    std::cin >> quan;
 
-    for (i = 1; i <= num; ++i)
+    for (int i = 1; i <= num; ++i)
+    {
+        for (int counter = 0; counter < quan + 1; ++counter)
         {
-            for (int counter = 0; counter < amount; ++counter)
-            {
-                for (int j = 0; j < num - i; j++) std::cout << " ";
-                for (int k = 0; k < i * 2 - 1; k++) std::cout << "*";
-                for (int j = 0; j < num - i; j++) std::cout << " ";
-            }
-                std::cout << std::endl;
-           
+            for (int j = 0; j < num - i; j++) std::cout << " ";
+            for (int k = 0; k < i * 2 - 1; k++) std::cout << "*";
+            for (int j = 0; j < num - i; j++) std::cout << " ";
         }
-            
-    i -= 2;
-    for (; i >= 0; --i)
+        std::cout << std::endl;
+
+    }
+
+    for (int i=num-1; i >= 0; --i)
+    {
+        for (int counter = 0; counter < quan + 1; ++counter)
         {
-            for (int counter = 0; counter < amount; ++counter)
-            {
-                for (int j = 0; j < num - i; ++j) std::cout << " ";
-                for (int k = 0; k < i * 2 - 1; ++k) std::cout << "*";
-                for (int j = 0; j < num - i; ++j) std::cout << " ";
-            }
-                std::cout << std::endl;
-            
+            for (int j = 0; j < num - i; ++j) std::cout << " ";
+            for (int k = 0; k < i * 2 - 1; ++k) std::cout << "*";
+            for (int j = 0; j < num - i; ++j) std::cout << " ";
+        }
+        std::cout << std::endl;
+
+    }
+    return 0;
+}
         }
     return 0;
     }
