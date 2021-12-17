c5dcabd656a81ba742a6f3814566a89c70a251a9
Abobus1k <91669954+Abobus1k@users.noreply.github.com>
2021-12-17 12:43:23 +0300

    Update task4.cpp

--- homework03/task4.cpp
+++ homework03/task4.cpp
@@ -3,24 +3,35 @@
 
 int main()
 {
-    std::cout << "Enter Height/2 of rhomb: ";
-int num, i;
+    std::cout << "Enter Height/2\n";
+    int num;
     std::cin >> num;
+    std::cout << "Enter quantity\n";
+    int quan;
+    std::cin >> quan;
 
-    for (i = 1; i <= num; i++)
+    for (int i = 1; i <= num; ++i)
+    {
+        for (int counter = 0; counter < quan + 1; ++counter)
         {
             for (int j = 0; j < num - i; j++) std::cout << " ";
             for (int k = 0; k < i * 2 - 1; k++) std::cout << "*";
-            std::cout << std::endl;
-        }
-
-    i -= 2;
-
-    for (; i >= 0; i--)
-        {
             for (int j = 0; j < num - i; j++) std::cout << " ";
-            for (int k = 0; k < i * 2 - 1; k++) std::cout << "*";
-            std::cout << std::endl;
         }
-    return 0;
+        std::cout << std::endl;
+
     }
+
+    for (int i=num-1; i >= 0; --i)
+    {
+        for (int counter = 0; counter < quan + 1; ++counter)
+        {
+            for (int j = 0; j < num - i; ++j) std::cout << " ";
+            for (int k = 0; k < i * 2 - 1; ++k) std::cout << "*";
+            for (int j = 0; j < num - i; ++j) std::cout << " ";
+        }
+        std::cout << std::endl;
+
+    }
+    return 0;
+}
