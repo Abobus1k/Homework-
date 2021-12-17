69e7ab5df818276c62ed4a6f3af708a2cbc75878
Abobus1k <91669954+Abobus1k@users.noreply.github.com>
2021-12-17 12:34:51 +0300

    Update task5.cpp

--- homework03/task5.cpp
+++ homework03/task5.cpp
@@ -6,13 +6,13 @@ int main()
     std::cout << "Enter Height/2\n";
     int num;
     std::cin >> num;
-    std::cout << "Enter quantity\n";
-    int quan;
-    std::cin >> quan;
+    std::cout << "Enter amount\n";
+    int amount;
+    std::cin >> amount;
 
     for (int i = 1; i <= num; ++i)
     {
-        for (int counter = 0; counter < quan + 1; ++counter)
+        for (int counter = 0; counter < amount + 1; ++counter)
         {
             for (int j = 0; j < num - i; j++) std::cout << " ";
             for (int k = 0; k < i * 2 - 1; k++) std::cout << "*";
@@ -24,7 +24,7 @@ int main()
 
     for (int i=num-1; i >= 0; --i)
     {
-        for (int counter = 0; counter < quan + 1; ++counter)
+        for (int counter = 0; counter < amount + 1; ++counter)
         {
             for (int j = 0; j < num - i; ++j) std::cout << " ";
             for (int k = 0; k < i * 2 - 1; ++k) std::cout << "*";
