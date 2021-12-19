d1e69c721c56091685981c16393cb47ce1b02717
Abobus1k <91669954+Abobus1k@users.noreply.github.com>
2021-12-19 17:14:35 +0300

    Update task4.cpp

--- bmp/task4.cpp
+++ bmp/task4.cpp
@@ -9,28 +9,22 @@ void testBoxFilter()
 
 
 
-void preconvolution(const char* name = "nokidsnoise.bmp", const char* out = "rezkost5.bmp")
-{
-	testBoxFilter();
-	RgbImg img = readRgbImg(name);
-	size_t krn_sz = 3;
-	double** ker = new double* [krn_sz];
-	for (int i = 0; i < krn_sz; ++i) ker[i] = new double[3];
-	for (int i = 0; i < krn_sz; ++i)
-		for (int j = 0; j < krn_sz; ++j)
-		{
-			std::cin >> ker[i][j];
-		}
-	writeRgbImg(out, convolution(img, krn_sz, ker));
-	deleteRgbImg(img);
-	delete[] ker;
-
-}
-
 int main(int argc, const char* argv[])
 {
 	try {
-		preconvolution();
+		testBoxFilter();
+		RgbImg img = readRgbImg("nokidsnoise.bmp");
+		size_t krn_sz = 3;
+		double** ker = new double* [krn_sz];
+		for (int i = 0; i < krn_sz; ++i) ker[i] = new double[3];
+		for (int i = 0; i < krn_sz; ++i)
+			for (int j = 0; j < krn_sz; ++j)
+			{
+				std::cin >> ker[i][j];
+			}
+		writeRgbImg("rezkosts.bmp", convolution(img, krn_sz, ker));
+		deleteRgbImg(img);
+		delete[] ker;
 	}
 	catch (std::exception const& e)
 	{
