a0f365684554ff02d425d196c93a50cfb2c236ea
Abobus1k <91669954+Abobus1k@users.noreply.github.com>
2021-12-19 15:01:16 +0300

    Add files via upload

---
+++ bmp/task2.cpp
@@ -0,0 +1,64 @@
+void rotation(int rotate = 180, const char* inpic = "rainbow.bmp", const char* outpic = "wobniar1.bmp")
+{
+	RgbImg in = readRgbImg(inpic);
+
+	if (rotate % 90 != 0)
+	{
+		std::cout << "Wrong angle!\n";
+	}
+	else {
+		rotate %= 360;
+		if (rotate == 90)
+		{
+			RgbImg out = createRgbImg(in.width, in.height, { 0, 0, 0 });
+			for (size_t x = 0; x < in.width; x++)
+			{
+				for (size_t y = 0; y < in.height; y++)
+				{
+					out.pixels[in.width - 1 - x][y] = in.pixels[y][x];
+				}
+			}
+			writeRgbImg(outpic, out);
+			deleteRgbImg(in);
+			deleteRgbImg(out);
+		}
+		else if (rotate == 180) {
+			RgbImg out = createRgbImg(in.height, in.width, { 0, 0, 0 });
+			for (size_t x = 0; x < in.width; x++)
+			{
+				for (size_t y = 0; y < in.height; y++)
+				{
+					out.pixels[y][x] = in.pixels[in.height - y - 1][in.width - x - 1];
+				}
+			}
+			writeRgbImg(outpic, out);
+			deleteRgbImg(in);
+			deleteRgbImg(out);
+		}
+		else if (rotate == 270) {
+			RgbImg out = createRgbImg(in.width, in.height, { 0, 0, 0 });
+			for (size_t x = 0; x < in.width; x++)
+			{
+				for (size_t y = 0; y < in.height; y++)
+				{
+					out.pixels[x][in.height - 1 - y] = in.pixels[y][x];
+				}
+			}
+			writeRgbImg(outpic, out);
+			deleteRgbImg(out);
+			deleteRgbImg(in);
+		}
+	}
+}
+int main(int argc, const char* argv[])
+{
+	try {
+		rotation();
+	}
+	catch (std::exception const& e)
+	{
+		std::cout << "Error: " << e.what() << std::endl;
+		return -1;
+	}
+	return 0;
+}
