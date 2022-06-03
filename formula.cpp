1632bf1e4a02d3a86008ef174a032218d4fa7c77
Abobus1k <91669954+Abobus1k@users.noreply.github.com>
2022-06-03 04:55:26 +0300

    Update formula.cpp

--- formula.cpp
+++ formula.cpp
@@ -1,7 +1,7 @@
 #include "formula.h"
 //-----------------------------------------------------
 FormulaNode* Formula::Postfix2Tree_(const char* str) {
-	int index = 0; // ????? ???????? ?? ??????? ??????
+	int index = 0; // Íîìåð ýëåìåíòà âî âõîäíîé ñòðîêå
 	std::stack<FormulaNode*> S;
 	char ch;
 	FormulaNode* result, * left, * right;
@@ -9,6 +9,8 @@ FormulaNode* Formula::Postfix2Tree_(const char* str) {
 		while ((ch = str[index]) != '\0') {
 			left = right = nullptr;
 			switch (ch) {
+			/*case '~':
+				flag = 1; break;*/
 			case '&':
 				if (S.empty()) throw 1; right = S.top(); S.pop();
 				if (S.empty()) throw 1; left = S.top(); S.pop();
@@ -25,9 +27,9 @@ FormulaNode* Formula::Postfix2Tree_(const char* str) {
 				result = new ImplicNode(left, right);
 				break;
 			case '~':
-				if (S.empty()) throw 1; right = S.top(); S.pop();
+				//if (S.empty()) throw 1; right = S.top(); S.pop();
 				if (S.empty()) throw 1; left = S.top(); S.pop();
-				result = new InversionNode(left, right);
+				result = new InvNode(left);
 				break;
 			case '<':
 				if (S.empty()) throw 1; right = S.top(); S.pop();
@@ -62,13 +64,21 @@ FormulaNode* Formula::Postfix2Tree_(const char* str) {
 					std::string res = "x";
 					while ((str[++index] >= '0') && (str[index] <= '9'))  res += str[index];
 					--index;
+					/*if (flag)
+					{	
+						flag = 0;
+						left = new ParamNode(res);
+						result = new InvNode(left);
+					}
+					else
+					{*/
 					result = new ParamNode(res);
 				}
 				else if (ch == '0')
 					result = new NumNode(0);
-				else if (ch <= '1')
+				else if(ch <= '1')
 					result = new NumNode(1);
-
+					
 
 			}
 			S.push(result);
@@ -95,7 +105,6 @@ FormulaNode* Formula::Postfix2Tree_(const char* str) {
 
 //-----------------------------------------------------
 const unsigned char Formula::ActionsTable[10][11] = {
-	//\0 v ~ & > < + | ^ = P
 	 { 7,2,2,2,2,2,2,2,2,2,1},// \0
 	 { 3,3,2,2,3,3,3,3,3,3,1},// v
 	 { 3,3,3,3,3,3,3,3,3,3,1},// ~
@@ -141,31 +150,49 @@ int Formula::ActionsColNumber(char ch) {
 }
 //-----------------------------------------------------
 void Formula::Infix2Postfix_(const char* in, char* out) {
-	int i = 0, j = 0; // i - ?????? ?? ??????? ??????, j - ? ????????
+	int i = 0, j = 0; // i - èíäåêñ âî âõîäíîé ñòðîêå, j - â âûõîäíîé
 	int row, col;
 	char prev;
+	//bool flag = 0;
 	unsigned char action;
 	std::stack<char> S;
 	do {
-		col = ActionsColNumber(in[i]);
-		row = S.empty() ? 0 : ActionsRowNumber(S.top());
-		action = ActionsTable[row][col];
-		switch (action) {
-		case 1: out[j] = in[i]; ++j; ++i; break;
-		case 2: S.push(in[i]); ++i; break;
-		case 3: out[j] = S.top(); ++j; S.pop(); break;
+			col = ActionsColNumber(in[i]);
+			row = S.empty() ? 0 : ActionsRowNumber(S.top());
+			action = ActionsTable[row][col];
+			switch (action) {
+			case 1: out[j] = in[i]; ++j; ++i; break;
+			case 2: S.push(in[i]); ++i; break;
+			case 3: out[j] = S.top(); ++j; S.pop(); break;
+			/*case 4:
+				if ((flag) && (in[i] == 'x'))
+				{
+					flag = 0;
+					out[j++] = S.top(); S.pop();
+					out[j++] = in[i]; break;
+				}
+				else if (flag)
+				{
+					out[j++] = in[i]; break;
+				}
+				else
+				{
+					flag = 1;
+					out[j++];
+				}
+				*/
 			//case 5: throw ErrorBracketsClose(instr, i); break;
-			//case 6: throw ErrorBracketsOpen(instr, i); break;
-		case 7: out[j] = '\0'; break;
+				//case 6: throw ErrorBracketsOpen(instr, i); break;
+			case 7: out[j] = '\0'; break;
 			//case 8: throw ErrorFunctionBrackets(instr, i); break;
-			//case 9: throw ErrorRValue(); break;
-		}
+				//case 9: throw ErrorRValue(); break;
+			}
 	} while (action != 7);
 
 }
 
 void Formula::InfixFilter_(const char* in, char* out) {
-	int i = 0, j = 0;
+	int i = 0, j = 0; 
 	char ch, prev = 0;
 	char buf[256]; int bufLen = 0;
 	while ((ch = in[i++]) != 0) {
@@ -176,31 +203,26 @@ void Formula::InfixFilter_(const char* in, char* out) {
 			continue;
 		}
 
-		if (ch == 'x')
+		if (ch =='x') 
 		{
 			buf[bufLen++] = ch;
 		}
 		else if (bufLen >= 1)//(x1 * x2)->0~(x1*x2)->0x1x2*~
 		{
-			if ((ch >= '0') && (ch <= '9'))
+			if((ch>='0')&&(ch<='9'))
 				buf[bufLen++] = ch;
 		}
-		else if (!(bufLen) && (ch != 'x'))
-		{
-			if ((ch == '~') && ((prev == 0) || ((prev == '(') && (bufLen == 0))))
-				out[j++] = '0';
-		}
 		else {
-
+			
 			if (bufLen > 1)
-			{
+			{ 
 				buf[bufLen] = '\0';
 				//outstr[j++] = FTable[FunNumberByName(buf)].ShortName;
 				//outstr[j++] = FunctionsTable::get(FunctionsTable::FunNumberByName(buf)).ShortName;
 
 				bufLen = 0;
 			}
-
+			
 		}
 		out[j++] = ch;
 		prev = ch;
@@ -209,3 +231,5 @@ void Formula::InfixFilter_(const char* in, char* out) {
 	//if (bufLen > 0) out[j++] = buf[0];
 	out[j] = '\0';
 }
+
+//-----------------------------------------------------------
